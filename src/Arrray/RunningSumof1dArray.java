9ee2658e9487f6ded1df7bd92871ca424d0f5c89
Akash <akashgond3112@gmail.com>
2022-12-02 20:08:13 +0000

    commit-021202022

---
+++ src/Arrray/RunningSumof1dArray.java
@@ -0,0 +1,17 @@
+package Arrray;
+
+public class RunningSumof1dArray {
+
+    public static int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++)
+            nums[i] += nums[i - 1];
+        return nums;
+    }
+
+    public static void main(String[] args) {
+
+        int result[] = runningSum(new int[] { 1, 2, 3, 4 });
+        System.out.println(result.toString());
+    }
+
+}
