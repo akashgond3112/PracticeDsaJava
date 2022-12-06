0781a5029ae3c7afaff424cca236a356b23b0768
Akash <akashgond3112@gmail.com>
2022-12-06 22:51:09 +0000

    commit-06122022

---
+++ src/Arrray/NumberofGoodPairs.java
@@ -0,0 +1,26 @@
+package Arrray;
+
+public class NumberofGoodPairs {
+
+    public static int numIdenticalPairs(int[] nums) {
+        int output = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] == nums[j] && nums[i] < nums[j]) {
+                    output++;
+                }
+            }
+        }
+        System.out.println(output);
+        return output;
+    }
+
+    public static void main(String[] args) {
+
+        numIdenticalPairs(new int[] { 1, 2, 3, 1, 1, 3 });
+    }
+
+}
