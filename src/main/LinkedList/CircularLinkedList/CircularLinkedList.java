b8c1c34edc6de8ec503e32b2633dbcf86aaf184d
akashgond3112 <akashgond3112@gmail.com>
2023-08-19 19:17:02 +0100

    updated the packages

---
+++ src/main/LinkedList/CircularLinkedList/CircularLinkedList.java
@@ -0,0 +1,57 @@
+package main.LinkedList.CircularLinkedList;
+
+public class CircularLinkedList {
+
+    static class Node{
+        int data;
+        Node next;
+    }
+
+    static Node push(Node head_ref, int data){
+        Node ptr1= new Node();
+        Node temp= head_ref;
+        ptr1.data=data;
+        ptr1.next=head_ref;
+
+
+        if(head_ref !=null){
+            while(temp.next != head_ref){
+                temp= temp.next;
+            }
+            temp.next=ptr1;
+        }else{
+            ptr1.next=ptr1;
+        }
+
+        head_ref= ptr1;
+        return head_ref;
+    }
+
+    static void printList(Node head){
+        Node temp= head;
+        if(head != null){
+            do{
+                System.out.print(temp.data+" ");
+                temp= temp.next;
+            }while(temp != head);
+        }
+    }
+
+    public static <ListNode> ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+
+        return  null;
+    }
+
+    public static void main(String[] args) {
+        Node head =null;
+        head= push(head,12);
+        head= push(head,13);
+        head= push(head,14);
+        head= push(head,15);
+
+        System.out.print("Content of circular linked list\n");
+        printList(head);
+    }
+}
---
+++ src/main/LinkedList/SinglyLinkedList/SearchElementInLinkedList.java
@@ -0,0 +1,73 @@
+package main.LinkedList.SinglyLinkedList;
+
+public class SearchElementInLinkedList {
+    Node head;
+
+    void push(int new_data) {
+
+        Node node = new Node(new_data);
+
+        node.next = head;
+        head = node;
+
+    }
+
+    boolean search(Node head, int data) {
+
+        Node current = head;
+        while (current != null) {
+            if (current.data == data) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    public boolean recursiveSearch(Node head, int x)
+    {
+        // Base case
+        if (head == null)
+            return false;
+
+        // If key is present in current node,
+        // return true
+        if (head.data == x)
+            return true;
+
+        // Recur for remaining list
+        return search(head.next, x);
+    }
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            next = null;
+        }
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+
+        // Start with the empty list
+        SearchElementInLinkedList llist = new SearchElementInLinkedList();
+
+        /*Use push() to construct below list
+        14->21->11->30->10  */
+        llist.push(10);
+        llist.push(30);
+        llist.push(11);
+        llist.push(21);
+        llist.push(14);
+
+        // Function call
+        if (llist.search(llist.head, 21))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
