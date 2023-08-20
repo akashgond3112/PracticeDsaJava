95f0d50d7e4b930b7ec34158efb3d84f77041ac5
agond <akash.gond@openbet.com>
2023-08-20 17:53:16 +0100

    Commit-20/08/2023

---
+++ src/Test/PascalTriangleTest.java
@@ -0,0 +1,31 @@
+package Test;
+
+
+import main.Arrray.easy.PascalTriangle;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PascalTriangleTest {
+
+    PascalTriangle pascalTriangle = new PascalTriangle();
+
+    @Test
+    public void TestOne() {
+        Assert.assertNotNull(pascalTriangle.soultion1(5));
+    }
+
+    @Test
+    public void TestTwo() {
+        Assert.assertNotNull(pascalTriangle.soultion2(7));
+    }
+
+    @Test
+    public void TestThree() {
+        Assert.assertNotNull(pascalTriangle.soultion1(0));
+    }
+
+    @Test
+    public void TestFOur() {
+        Assert.assertNotNull(pascalTriangle.soultion1(-1));
+    }
+}
