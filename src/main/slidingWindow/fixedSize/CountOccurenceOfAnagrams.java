64dede3325bcada523df5b7d1c4f2bfb8c000268
agond <akash.gond@openbet.com>
2023-08-25 00:00:01 +0100

    Commit on 2023-08-25

---
+++ src/main/slidingWindow/fixedSize/CountOccurenceOfAnagrams.java
@@ -0,0 +1,13 @@
+package main.slidingWindow.fixedSize;
+
+public class CountOccurenceOfAnagrams {
+
+    /*Expected Output
+    Input:
+    txt = forxxorfxdofr
+    pat = for
+    Output: 3
+    Explanation: for, orf and ofr appears
+    in the txt, hence answer is 3.
+    */
+}
