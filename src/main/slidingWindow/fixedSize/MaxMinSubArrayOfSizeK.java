cdb340f00fe1f8077d0efab064265c13da283810
agond <akash.gond@openbet.com>
2023-08-28 23:02:06 +0100

    Commit-28/08/2023

--- src/main/slidingWindow/fixedSize/MaxMinSubArrayOfSizeK.java
+++ src/main/slidingWindow/fixedSize/MaxMinSubArrayOfSizeK.java
@@ -45,7 +45,7 @@ public class MaxMinSubArrayOfSizeK {
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, 4, 2, 10, 2, 3, 1, 0, 20};
+        int[] arr = {1, 4, 2, 10, 2, 3, 1, 0, 20};
         int k = 2;
         System.out.println(solutionMax(arr, k));
         System.out.println(solutionMin(arr, k));
---
+++ src/main/slidingWindow/variableSize/LargestSubarrayOfSumK.java
@@ -0,0 +1,82 @@
+package main.slidingWindow.variableSize;
+
+import java.util.HashMap;
+
+public class LargestSubarrayOfSumK {
+
+    public static int solutionMax(int[] arr, int expectedSum) {
+        int actualSum = 0;
+        int startIndex = 0;
+        int endIndex = 0;
+        int max = Integer.MIN_VALUE; // Initialize max with a smaller value
+
+        while (endIndex < arr.length) {
+            actualSum = actualSum + arr[endIndex];
+
+            if (actualSum < expectedSum) {
+                endIndex++;
+            } else if (actualSum == expectedSum) {
+                max = Math.max(max, endIndex - startIndex + 1);
+                endIndex++;
+            } else if( actualSum > expectedSum){
+                while (actualSum > expectedSum) {
+                    actualSum = actualSum - arr[startIndex];
+                    startIndex++;
+                }
+                if (actualSum == expectedSum) {
+                    max = Math.max(max, endIndex - startIndex + 1);
+                }
+                endIndex++;
+
+            }
+
+        }
+        return max;
+    }
+
+    public static int solutionMax(int arr[], int n, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int max = 0;
+        int sum = 0;
+
+        for(int i=0;i<n;i++){
+            sum += arr[i];
+            if(sum == k){
+                max = Math.max(i+1 , max);
+            }
+            else if(map.containsKey(sum-k)){
+                max = Math.max(i-map.get(sum-k) , max);
+            }
+            if(!(map.containsKey(sum))){
+                map.put(sum, i);
+
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = {1, 2, 3, 4, 5};
+        int expectedSum1 = 9;
+//        System.out.println("Test 1: " + solutionMax(test1, expectedSum1)); // Output: 2 (Sub-array: [2, 3, 4])
+
+        int[] test2 = {-1, 2, 3, -4, 5};
+        int expectedSum2 = 6;
+//        System.out.println("Test 2: " + solutionMax(test2, expectedSum2)); // Output: 4 (Sub-array: [2, 3, -4, 5])
+        System.out.println("Test 2: " + solutionMax(test2,test2.length, expectedSum2)); // Output: 4 (Sub-array: [2, 3, -4, 5])
+
+        int[] test3 = {4, -1, 2, 3, -4, 5};
+        int expectedSum3 = 4;
+//        System.out.println("Test 3: " + solutionMax(test3, expectedSum3)); // Output: 4 (Sub-array: [4])
+
+        int[] test4 = {-2, -3, 4, -1, -2, 1, 5, -3};
+        int expectedSum4 = 0;
+//        System.out.println("Test 4: " + solutionMax(test4, expectedSum4)); // Output: 5 (Sub-array: [-2, -3, 4, -1, -2])
+
+        int[] test5 = {1, 1, 1, 1, 1, 1, 1, 1};
+        int expectedSum5 = 4;
+//        System.out.println("Test 5: " + solutionMax(test5, expectedSum5)); // Output: 4 (Sub-array: [1, 1, 1, 1])
+    }
+
+}
