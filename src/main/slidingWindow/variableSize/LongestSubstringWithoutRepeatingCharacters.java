b44dd8c8113ba25981d488e1443f64164e15ae6d
agond <akash.gond@openbet.com>
2023-08-29 23:42:10 +0100

    Commit-29/08/2023

--- src/main/slidingWindow/variableSize/LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/slidingWindow/variableSize/LongestSubstringWithoutRepeatingCharacters.java
@@ -3,26 +3,27 @@ package main.slidingWindow.variableSize;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LongestKUniqueCharactersSubstring {
+public class LongestSubstringWithoutRepeatingCharacters {
 
-    public static int longestKSubstr(String s, int k) {
-        if (s.length() < k) return -1;
+    public static int longestKSubstr(String s) {
+        if(s.isEmpty()) return 0;
         int startIndex = 0;
         int endIndex = 0;
         int max = Integer.MIN_VALUE; // Initialize max with a smaller value
+
         Map<Character, Integer> characterHashMap = new HashMap<>();
 
         while (endIndex < s.length()) {
             char ch = s.charAt(endIndex);
             characterHashMap.put(ch, characterHashMap.getOrDefault(ch, 0) + 1);
 
-            if (characterHashMap.size() < k) {
+            if (characterHashMap.size() > (endIndex - startIndex + 1)) {
                 endIndex++;
-            } else if (characterHashMap.size() == k) {
+            } else if (characterHashMap.size() == (endIndex - startIndex + 1)) {
                 max = Math.max(max, endIndex - startIndex + 1);
                 endIndex++;
             } else {
-                while (characterHashMap.size() > k) {
+                while (characterHashMap.size() < (endIndex - startIndex + 1)) {
 
                     if (characterHashMap.containsKey(s.charAt(startIndex))) {
                         int freq = characterHashMap.get(s.charAt(startIndex));
@@ -37,12 +38,12 @@ public class LongestKUniqueCharactersSubstring {
                 endIndex++;
             }
         }
-        return (characterHashMap.size() < k) ? 0 : max;
+        return  max;
     }
 
     public static void main(String[] args) {
-        String txt = "zzkn";
-        System.out.println(longestKSubstr(txt, 7));
+        String txt = "bbbbb";
+        System.out.println(longestKSubstr(txt));
 
     }
 
