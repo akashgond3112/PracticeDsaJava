febedee7b8766f8bebe334a3263274a76e2d7beb
agond <akash.gond@openbet.com>
2023-10-03 00:00:01 +0100

    Commit on 2023-10-03

---
+++ src/main/slidingWindow/medium/MaximumLengthOfRepeatedSubarray.java
@@ -0,0 +1,33 @@
+package main.slidingWindow.medium;
+
+/**
+ * @author agond
+ * @package main.slidingWindow.medium
+ * @Date 02/10/2023
+ * @Project PracticeDSA
+ *
+ * 718. Maximum Length of Repeated Subarray
+ * Medium
+ * Topics
+ * Companies
+ * Hint
+ * Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears in both arrays.
+ *
+ * Example 1:
+ *
+ * Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+ * Output: 3
+ * Explanation: The repeated subarray with maximum length is [3,2,1].
+ * Example 2:
+ *
+ * Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+ * Output: 5
+ * Explanation: The repeated subarray with maximum length is [0,0,0,0,0].
+ *
+ * Constraints:
+ *
+ * 1 <= nums1.length, nums2.length <= 1000
+ * 0 <= nums1[i], nums2[i] <= 100
+ */
+public class MaximumLengthOfRepeatedSubarray {
+}
