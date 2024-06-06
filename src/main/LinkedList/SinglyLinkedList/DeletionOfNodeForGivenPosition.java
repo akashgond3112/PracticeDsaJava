85107d668c105197afe6222e74d628b4dfa17994
akashgond3112 <akashgond3112@gmail.com>
2024-06-06 23:00:25 +0100

    Commit-06/06/2024

--- src/main/LinkedList/SinglyLinkedList/DeletionOfNodeForGivenPosition.java
+++ src/main/LinkedList/SinglyLinkedList/DeletionOfNodeForGivenPosition.java
@@ -1,78 +1,112 @@
 package main.LinkedList.SinglyLinkedList;
 
+/*
+19. Remove Nth Node From End of List
+Solved
+Medium
+Topics
+Companies
+Hint
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+
+Example 2:
+Input: head = [1], n = 1
+Output: []
+
+Example 3:
+Input: head = [1,2], n = 1
+Output: [1]
+
+Constraints:
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+*/
+
 public class DeletionOfNodeForGivenPosition {
 
-    Node head;
+	Node head;
 
-    void push(int new_Data) {
-        Node new_node = new Node(new_Data);
+	void push(int new_Data) {
+		Node new_node = new Node(new_Data);
 
-        new_node.next = head;
-        head = new_node;
-    }
+		new_node.next = head;
+		head = new_node;
+	}
 
-    void delete(int position) {
+	Node delete(int position) {
 
-        if (head == null) {
-            return;
-        }
+		if (head == null) {
+			return null;
+		}
 
-        Node temp = head;
+		Node dummy = new Node(0);
+		dummy.next = head;
+		Node fast = dummy;
+		Node slow = dummy;
 
-        if (position == 0) {
-            head = temp.next;
-            return;
-        }
+		// Move fast ahead by n+1 steps
+		for (int i = 0; i <= position; i++) {
+			fast = fast.next;
+		}
 
-        for (int i = 0; temp != null && i < position - 1; i++) {
-            temp = temp.next;
-        }
+		// Move both fast and slow pointers until fast reaches the end
+		while (fast != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
 
-        if (temp == null && temp.next == null) {
-            return;
-        }
+		// Remove the nth node from end
+		slow.next = slow.next.next;
 
-        Node next = temp.next.next;
+		return dummy.next;
+	}
 
-        temp.next = next;
-    }
+	void printList() {
+		Node tNode = head;
+		while (tNode != null) {
+			System.out.print(tNode.data + " ");
+			tNode = tNode.next;
+		}
+	}
 
-    void printList() {
-        Node tNode = head;
-        while (tNode != null) {
-            System.out.print(tNode.data + " ");
-            tNode = tNode.next;
-        }
-    }
+	class Node {
+		int data;
+		Node next;
 
-    class Node {
-        int data;
-        Node next;
+		Node(int d) {
+			data = d;
+			next = null;
+		}
 
-        Node(int d) {
-            data = d;
-            next = null;
-        }
-    }
+	}
 
-    public static void main(String[] args) {
 
-        DeletionOfNodeForGivenPosition data = new DeletionOfNodeForGivenPosition();
+	public static void main(String[] args) {
 
-        data.push(1);
-        data.push(2);
-        data.push(3);
-        data.push(4);
-        data.push(5);
-        data.push(6);
+		DeletionOfNodeForGivenPosition data = new DeletionOfNodeForGivenPosition();
 
-        System.out.println("\n Create linked list is: ");
-        data.printList();
+		data.push(1);
+		data.push(2);
+//		data.push(3);
+//		data.push(4);
+//		data.push(5);
 
-        data.delete(1);
+		System.out.println("\n Create linked list is: ");
+		data.printList();
 
-        System.out.println("\n Linked list after deletion is: ");
-        data.printList();
-    }
+		Node delete = data.delete(1);
+
+		System.out.println("\n Updated linked list is: ");
+		while (delete != null) {
+			System.out.print(delete.data + " ");
+			delete = delete.next;
+		}
+	}
 
 }
