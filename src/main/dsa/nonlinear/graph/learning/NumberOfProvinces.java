29e2c2977fa282b98fea2753cc2e8fc30096e879
akashgond3112 <akashgond3112@gmail.com>
2024-07-10 22:30:40 +0100

    Commit-10/07/2024

--- src/main/dsa/nonlinear/graph/learning/NumberOfProvinces.java
+++ src/main/dsa/nonlinear/graph/learning/NumberOfProvinces.java
@@ -45,4 +45,48 @@ public class NumberOfProvinces {
 	public static void main(String[] args) {
 
 	}
+
+	public static class FloodFillAlgorithm {
+
+		public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+
+			int initialColor = image[sr][sc];
+			int[][] ans = image;
+			int[] delRow = { -1, 0, +1, 0 };
+			int[] delCol = { 0, +1, 0, -1 };
+			dfs(sr, sc, ans, image, newColor, delRow, delCol, initialColor);
+			return ans;
+
+		}
+
+		private void dfs(int sr, int sc, int[][] image, int[][] ans, int newColor, int[] delRow, int[] delCol,
+				int initialColor) {
+
+			ans[sr][sc] = newColor;
+			int n = image.length;
+			int m = image[0].length;
+
+			for (int k = 0; k < 4; k++) {
+				int newRow = sr + delRow[k];
+				int newCol = sc + delCol[k];
+
+				if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && image[newRow][newCol] == initialColor && ans[newRow][newCol] != newColor) {
+					dfs(newRow, newCol, ans, image, newColor, delRow, delCol, initialColor);
+				}
+			}
+		}
+
+		public static void main(String[] args) {
+			int[][] image = { { 1, 1, 1 }, { 1, 1, 0 }, { 1, 0, 1 } };
+
+			// sr = 1, sc = 1, newColor = 2
+			FloodFillAlgorithm obj = new FloodFillAlgorithm();
+			int[][] ans = obj.floodFill(image, 1, 1, 2);
+			for (int i = 0; i < ans.length; i++) {
+				for (int j = 0; j < ans[i].length; j++)
+					System.out.print(ans[i][j] + " ");
+				System.out.println();
+			}
+		}
+	}
 }
