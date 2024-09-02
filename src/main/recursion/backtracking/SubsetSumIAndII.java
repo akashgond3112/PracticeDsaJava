afbea74e8cca6731946e14f2485a242de2109e86
akashgond3112 <akashgond3112@gmail.com>
2024-09-02 19:25:23 +0100

    Commit-02/09/2024

---
+++ src/main/recursion/backtracking/SubsetSumIAndII.java
@@ -0,0 +1,54 @@
+package main.recursion.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SubsetSumIAndII {
+
+	public static void subsetI(int[] arr, int sum, int index, ArrayList<Integer> result) {
+
+		if (index == arr.length) {
+			result.add(sum);
+			return;
+		}
+
+		subsetI(arr, sum + arr[index], index + 1, result);
+		subsetI(arr, sum, index + 1, result);
+	}
+
+	public static ArrayList<Integer> getSubsetSumI(int[] arr) {
+		ArrayList<Integer> result = new ArrayList<>();
+		subsetI(arr, 0, 0, result);
+		Collections.sort(result);
+		return result;
+	}
+
+	private static void subsetII(int[] arr, int index, ArrayList<ArrayList<Integer>> result, ArrayList<Integer> nums) {
+		result.add(new ArrayList<>(nums));
+
+		for (int i = index; i < arr.length; i++) {
+			if (i != index && arr[i] == arr[i - 1]) {
+				continue;
+			}
+			nums.add(arr[i]);
+
+			subsetII(arr, i + 1, result, nums);
+			nums.remove(nums.size() - 1);
+		}
+	}
+
+	public static ArrayList<ArrayList<Integer>> getSubsetSumII(int[] arr) {
+		Arrays.sort(arr);
+		ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+		ArrayList<Integer> nums = new ArrayList<>();
+		subsetII(arr, 0, result, nums);
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3 };
+		System.out.println(getSubsetSumI(arr));
+		System.out.println(getSubsetSumII(arr));
+	}
+}
