a7f9e7f9180a8b0db5d4cf8a4e91236ec5802e3d
akashgond3112 <akashgond3112@gmail.com>
2024-10-12 16:17:18 +0100

    Commit-12/09/2024

--- src/main/dsa/linear/stack/MinStack.java
+++ src/main/dsa/linear/stack/MinStack.java
@@ -1,6 +1,9 @@
 package main.dsa.linear.stack;
 
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 /**
  * Your MinStack object will be instantiated and called as such: MinStack obj = new MinStack(); obj.push(val);
  * obj.pop(); int param_3 = obj.top(); int param_4 = obj.getMin();
@@ -33,26 +36,89 @@ package main.dsa.linear.stack;
 
 class MinStack {
 
-	public MinStack() {
+	static class Pair<I extends Number, I1 extends Number> {
+		int min;
+		int val;
 
+		public Pair(int min, int val) {
+			this.min = min;
+			this.val = val;
+		}
 	}
 
-	public void push(int val) {
+	Stack<Pair<Integer, Integer>> stack = new Stack<>();
+	Stack<Long> st = new Stack<>();
 
+	Long mini = Long.MAX_VALUE;
+
+	public void push(int val) {
+		if (stack.isEmpty()) {
+			stack.push(new Pair<>(val, val));
+		} else {
+			stack.push(new Pair<>(val, Math.min(val, stack.peek().val)));
+		}
 	}
 
 	public void pop() {
-
+		if (stack.isEmpty()) {
+			throw new EmptyStackException();
+		} else {
+			stack.pop().val
+		}
 	}
 
 	public int top() {
-
-		return 0;
+		if (stack.isEmpty()) {
+			throw new EmptyStackException();
+		} else {
+			return stack.peek().val;
+		}
 	}
 
 	public int getMin() {
-		return 0;
+		if (stack.isEmpty()) {
+			throw new EmptyStackException();
+		}
+		return stack.peek().min;
 
+	}
+
+
+	public void pushNew(int value) {
+		Long val = (long) value;
+		if (st.isEmpty()) {
+			mini = val;
+			st.push(val);
+		} else {
+			if (val < mini) {
+				st.push(2 * val - mini);
+				mini = val;
+			} else {
+				st.push(val);
+			}
+		}
+	}
+
+	public void popNew() {
+		if (st.isEmpty())
+			return;
+
+		Long val = st.pop();
+		if (val < mini) {
+			mini = 2 * mini - val;
+		}
+	}
+
+	public int topNew() {
+		Long val = st.peek();
+		if (val < mini) {
+			return mini.intValue();
+		}
+		return val.intValue();
+	}
+
+	public int getMinNew() {
+		return mini.intValue();
 	}
 
 	public static void main(String[] args) {
