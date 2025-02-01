726b16d94e94387b83c0a4734b8a18f105094906
akashgond3112 <akashgond3112@gmail.com>
2025-02-01 12:31:25 +0000

    Commit : 01/02/2025

--- src/main/blind75/dp/LongestIncreasingSubsequence.java
+++ src/main/blind75/dp/LongestIncreasingSubsequence.java
@@ -107,4 +107,30 @@ public class LongestIncreasingSubsequence {
 			return Arrays.stream(LIS).max().getAsInt();
 		}
 	}
+
+
+	/**
+	 * Time complexity: O(nlogn) Space complexity: O(n)
+	 */
+	public static class SolutionUsingBinarySearch {
+		public int lengthOfLIS(int[] nums) {
+			List<Integer> dp = new ArrayList<>();
+			dp.add(nums[0]);
+
+			int LIS = 1;
+			for (int i = 1; i < nums.length; i++) {
+				if (dp.get(dp.size() - 1) < nums[i]) {
+					dp.add(nums[i]);
+					LIS++;
+					continue;
+				}
+
+				int idx = Collections.binarySearch(dp, nums[i]);
+				if (idx < 0) idx = -idx - 1;
+				dp.set(idx, nums[i]);
+			}
+
+			return LIS;
+		}
+	}
 }
