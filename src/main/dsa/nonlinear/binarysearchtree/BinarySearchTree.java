558c071c57cc24438302227ffcc6ba9feeff5cee
akashgond3112 <akashgond3112@gmail.com>
2025-03-24 20:10:57 +0000

    Commit : 24/03/2025

---
+++ src/main/dsa/nonlinear/binarysearchtree/BinarySearchTree.java
@@ -0,0 +1,66 @@
+package main.dsa.nonlinear.binarysearchtree;
+
+public class BinarySearchTree {
+
+	public Node insert(Node root, int key) {
+		if (root == null) {
+			return new Node(key);
+		}
+
+		if (key < root.key) {
+			root.left = insert(root.left, key);
+		} else if (key > root.key) {
+			root.right = insert(root.right, key);
+		}
+
+		return root;
+	}
+
+	public Node search(Node root, int key) {
+		if (root == null || root.key == key) {
+			return root;
+		}
+
+		if (key < root.key) {
+			return search(root.left, key);
+		}
+
+		return search(root.right, key);
+	}
+
+	public Node delete(Node root, int key) {
+		if (root == null) {
+			return root;
+		}
+
+		if (key < root.key) {
+			root.left = delete(root.left, key);
+		} else if (key > root.key) {
+			root.right = delete(root.right, key);
+		} else {
+			// Node with only one child or no child
+			if (root.left == null) {
+				return root.right;
+			} else if (root.right == null) {
+				return root.left;
+			}
+
+			// Node with two children: Get the inorder successor
+			root.key = minValue(root.right);
+
+			// Delete the inorder successor
+			root.right = delete(root.right, root.key);
+		}
+
+		return root;
+	}
+
+	private int minValue(Node root) {
+		int minValue = root.key;
+		while (root.left != null) {
+			minValue = root.left.key;
+			root = root.left;
+		}
+		return minValue;
+	}
+}
---
+++ src/main/dsa/nonlinear/binarysearchtree/Node.java
@@ -0,0 +1,11 @@
+package main.dsa.nonlinear.binarysearchtree;
+
+public class Node {
+	int key;
+	Node left, right;
+
+	public Node(int item) {
+		key = item;
+		left = right = null;
+	}
+}
